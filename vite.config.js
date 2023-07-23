915fc31909f45b2f76158213ced47662aa95138e
d3lt409 <manuelfer1996@gmail.com>
2023-07-23 15:56:08 -0500

    server api change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://d3lt409.github.io/ReactFastApiClient"
+  base:"https://d3lt409.github.io/"
 })
