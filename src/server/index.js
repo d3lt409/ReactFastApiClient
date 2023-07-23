f2e69d3a7a51b65a3c89005b5f742e31f1a8d68d
d3lt409 <manuelfer1996@gmail.com>
2023-07-23 15:48:42 -0500

    server api change

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_API = "http://localhost:8000/api/v1";
+const URL_API = "https://fastapimongo.onrender.com/api/v1";
  
 export const taskAxios = axios.create({
     baseURL:`${URL_API}/tasks/`
